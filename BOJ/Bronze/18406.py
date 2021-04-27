813b2d02357beb1b29ec4616de889117563135e9
HYEWON LEE <hyewon328@naver.com>
2021-04-27 22:03:17 +0900

    Create 18406.py

---
+++ BOJ/Bronze/18406.py
@@ -0,0 +1,12 @@
+n = input()
+
+left_sum = 0; right_sum = 0
+for i in range(len(n)//2):
+    left_sum += int(n[i])
+for j in range(len(n)//2, len(n)):
+    right_sum += int(n[j])
+    
+if left_sum == right_sum:
+    print('LUCKY')
+else:
+    print('READY')
